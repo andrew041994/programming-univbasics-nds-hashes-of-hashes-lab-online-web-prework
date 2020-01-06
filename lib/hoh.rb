3f41f061383ff53b6ec6c4680aa1be007a869a72
Alehandro Persaud <andrew041994@gmail.com>
2020-01-06 05:01:44 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,10 +49,11 @@ def naming_system
           :label=>"Class",
             :sub_category=>{
               :label=>"Order",
+                :sub_category=>{
                 :label=>"Family",
                   :sub_category=>{
                     :label=>"Genus",
                       :label=>"Species",
                         :sub_category=>nil
-                      }}}}}
+                      }}}}}}
     end
